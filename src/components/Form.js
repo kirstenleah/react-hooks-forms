5a79bf22664f551db75951c4be5682c134510e89
Kirsten <kirsten.staggs@gmail.com>
2022-04-29 22:13:53 -0600

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,45 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
+  return (
+    <form>
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+  const [newsletter, setNewsletter] = useState(false);
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value)
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+  }
+
+  function handleNewsletterChange(event) {
+    setNewsletter(event.target.checked)
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input 
+      type="checkbox"
+      id="newsletter"
+      onChange={handleNewsletterChange}
+      checked={newsletter}
+      />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
